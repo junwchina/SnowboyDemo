cbc5cfdfad588f6298b8e673f11621fca3433be0
wangjun <elexjun@gmail.com>
2023-07-06 17:00:04 +0800

    测试你好小静

---
+++ app/src/main/java/ai/kitt/snowboy/Constants.java
@@ -0,0 +1,16 @@
+package ai.kitt.snowboy;
+import java.io.File;
+import android.os.Environment;
+
+public class Constants {
+    public static final String ASSETS_RES_DIR = "snowboy";
+    public static final String DEFAULT_WORK_SPACE = "/snowboy/";
+    public static final String ACTIVE_UMDL = "hello_xiaojing.pmdl";
+    public static final String ACTIVE_RES = "common.res";
+
+    public static final String DING_WAV = "ding.wav";
+    public static final String SAVE_AUDIO = "recording.pcm";
+    public static final int SAMPLE_RATE = 16000;
+
+
+}
